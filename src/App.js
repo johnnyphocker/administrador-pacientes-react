1be66e81d05a0a933aa4766ca95824a0195042a1
JP <johnny.phocker@gmail.com>
2018-08-03 18:54:41 -0600

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Header from './componentes/header';
+import AgregarCita from './componentes/agregarCita';
+import ListaCitas from './componentes/listaCitas';
+
+class App extends Component {
+
+  state = {
+    citas: {}
+  }
+
+  componentDidMount() {
+    const citasLS = localStorage.getItem('citas');
+    if(citasLS) {
+      this.setState({citas: JSON.parse(citasLS)})
+    }
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem('citas', JSON.stringify(this.state.citas))
+  }
+
+  crearCita = infoCita => {
+    const citas = {...this.state.citas};
+
+    citas[`citas${Date.now()}`] = infoCita;
+
+    this.setState({citas});
+  }
+
+  borrarCita = id => {
+    const citas = {...this.state.citas};
+
+    delete citas[id];
+
+    this.setState({citas});
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Header titulo="Administrador de pacientes" />
+        <div className="row">
+          <div className="col-md-6">
+            <AgregarCita
+              crearCita={this.crearCita}
+            />
+          </div>
+          <div className="col-md-6">
+            <ListaCitas
+              citas={this.state.citas}
+              borrarCita={this.borrarCita}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
